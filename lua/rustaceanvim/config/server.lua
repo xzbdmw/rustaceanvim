4111dbc37f31a0f433e693a05cfc422021c76e3c
xzb <2598514867@qq.com>
2024-07-23 23:01:31 +0800

    feat(diagnostic): ansi code colored disagnostics in floating and split window

--- lua/rustaceanvim/config/server.lua
+++ lua/rustaceanvim/config/server.lua
@@ -73,6 +73,7 @@ local function make_rustaceanvim_capabilities()
   -- send actions with hover request
   capabilities.experimental = {
     hoverActions = true,
+    colorDiagnosticOutput = true,
     hoverRange = true,
     serverStatusNotification = true,
     snippetTextEdit = true,
